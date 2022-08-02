335aec2434bd63465720a6e5cf37b0a5a26d00f1
milol bayegui <clooney.lysandrepro@gmail.com>
2022-08-02 05:19:29 +0100

    this is the first push

---
+++ FRESHVOTE/src/main/java/com/fstg/FRESHVOTE/FreshvoteApplication.java
@@ -0,0 +1,13 @@
+package com.fstg.FRESHVOTE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshvoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshvoteApplication.class, args);
+	}
+
+}
---
+++ FRESHVOTE/src/test/java/com/fstg/FRESHVOTE/FreshvoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fstg.FRESHVOTE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshvoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
