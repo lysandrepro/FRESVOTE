2f955d9a9af4a40a7ea0c0b97eca9367a3565445
clooney lysandre <clooney.lysandre@gmail.com>
2022-08-13 02:42:09 +0100

    this is my first controller in this project

---
+++ FRESHVOTE/src/main/java/com/fstg/FRESHVOTE/domain/DashbordController.java
@@ -0,0 +1,16 @@
+package com.fstg.FRESHVOTE.domain;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class DashbordController {
+	
+	@RequestMapping(value="/",method=RequestMethod.GET)
+	public String rootView() {
+		
+		return "index";
+	}
+
+}
